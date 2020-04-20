64f3abf1d02d79b123c716beefe611d03913bdbb
razvan <razvan.maftei@yahoo.com>
2020-04-20 13:47:21 +0300

    new pom

---
+++ src/test/java/com/maftraz/servicecarsapp/ServicecarsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maftraz.servicecarsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicecarsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
